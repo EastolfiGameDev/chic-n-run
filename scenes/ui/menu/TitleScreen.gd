f83f9ecc703aed54da5b7c0c8cc6b89c3d1873da
Eduardo Astolfi <eastolfi91@gmail.com>
2020-06-11 22:27:36 +0200

    fix: fix a problem with the game onver

--- scenes/ui/menu/TitleScreen.gd
+++ scenes/ui/menu/TitleScreen.gd
@@ -5,9 +5,10 @@ onready var max_score: Label = $MarginContainer/HBoxContainer/TitleImageContaine
 func _ready() -> void:
     var score = Game.get_max_score()
     max_score.text = str(score)
+    get_tree().paused = false
 
 func _on_PlayButton_pressed() -> void:
-    get_tree().change_scene("res://scenes/levels/grass/GrassLevel.tscn")
+    get_tree().change_scene("res://scenes/levels/grass/BeachLevel.tscn")
 
 func _on_CollectionButton_pressed() -> void:
     get_tree().change_scene("res://scenes/ui/menu/CollectionScreen.tscn")
