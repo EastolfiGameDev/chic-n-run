a2f14f8540ed35c4603c28d57e652d6d9fcaccf6
Eduardo Astolfi <eastolfi91@gmail.com>
2020-06-11 22:51:34 +0200

    first try of skins

--- scenes/ui/menu/CollectionScreen.gd
+++ scenes/ui/menu/CollectionScreen.gd
@@ -1,6 +1,14 @@
 extends Control
 
-
+const background = []
 
 func _on_BackButton_pressed() -> void:
     get_tree().change_scene("res://scenes/ui/menu/TitleScreen.tscn")
+
+
+func _on_PreviousBackground_pressed() -> void:
+    pass # Replace with function body.
+
+
+func _on_NextBackground2_pressed() -> void:
+    pass # Replace with function body.
